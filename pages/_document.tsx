489d0f773cb99bfd7fc819748bfcf74bab937160
durimfe <durim.dev@gmail.com>
2023-05-02 16:34:56 +0200

    changed font-family

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
       </Head>
